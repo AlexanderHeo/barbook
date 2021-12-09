7fbbf0d414c7d0d559842b8fffc2db2481d2a18d
Alexander Heo <jikster712@gmail.com>
2021-12-09 12:48:32 -0800

    feature: list components renamed

---
+++ components/drinkList.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import styles from './drinkList.module.css';
+
+export default function List({ children }) {
+  return (
+    <>
+      <h3 className={styles.header3}>{children.type}</h3>
+      <div className={styles.wrapper}>
+        {children.drinkList.map((drink) => {
+          return (
+            <button key={drink.id} className={styles.drinkButton}>
+              <Link href={`/cocktails/${drink.name}`} cocktail={drink.name}>
+                <p>{drink.name}</p>
+              </Link>
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+}
